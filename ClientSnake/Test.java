9affbca9b76e02979c809f67aed45e680f5a2aec
bessantoy <bessantoy@gmail.com>
2023-02-01 15:04:11 +0100

    first commit

---
+++ ClientSnake/Test.java
@@ -0,0 +1,15 @@
+import controller.ControllerSnakeGame;
+import controller.ControllerSimpleGame;
+import model.SnakeGame;
+import model.SimpleGame;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		// ControllerSimpleGame controllerSimpleGame = new ControllerSimpleGame();
+
+		ControllerSnakeGame controllerSnakeGame = new ControllerSnakeGame();
+
+	}
+}
